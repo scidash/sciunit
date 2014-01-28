a037bac3abca961a931c2a38d08e46f7cd32813d
Cyrus Omar <cyology@gmail.com>
2014-01-28 15:31:52 -0500

    Small changes to example models

--- sciunit/models.py
+++ sciunit/models.py
@@ -4,13 +4,11 @@ from random import uniform
 class ConstModel(sciunit.Model, 
 					 sciunit.capabilities.ProducesNumber):
 	"""A model that always produces a constant number as output."""
-	
 	def __init__(self,constant):
 		self.constant = constant 
 		
-	def produce_data(self):
+	def produce_number(self):
 		return self.constant
-
 
 class UniformModel(sciunit.Model,
 					   sciunit.capabilities.ProducesNumber):
@@ -18,7 +16,7 @@ class UniformModel(sciunit.Model,
 	in [a,b] as output."""
 	
 	def __init__(self,a,b):
-		self.value = uniform(a,b)
+		self.a, self.b = a, b
 	
-	def produce_data(self):
-		return self.value
+	def produce_number(self):
+		return uniform(self.a, self.b)
