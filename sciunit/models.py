76051f0f489ab8e978699bf967ec13b698ee57ad
Richard C Gerkin <rgerkin@asu.edu>
2014-04-13 16:14:27 -0700

    Added in memory model run caching

--- sciunit/models.py
+++ sciunit/models.py
@@ -1,5 +1,7 @@
 import sciunit
 from random import uniform
+import json
+from datetime import datetime
 
 class ConstModel(sciunit.Model, 
 					 sciunit.capabilities.ProducesNumber):
@@ -22,3 +24,44 @@ class UniformModel(sciunit.Model,
 	
 	def produce_number(self):
 		return uniform(self.a, self.b)
+
+def runcache(func):
+	"""A decorator used on a model method which calls the model's run() method
+	if the model has not been run using the current arguments or simply sets
+	model attributes to match the run results if it has."""  
+
+	def decorate(*args, **kwargs):
+		model = args[0] # Assumed to be self.  
+		assert hasattr(model,'run'), "Model must have a run method."
+		if func.__name__ == 'run': # Run itself.  
+			run_args = kw_args
+		else: # Any other method.  
+			run_args = kwargs['run'] if 'run' in kwargs else {}
+		if not hasattr(model.__class__,'cached_runs'):
+			model.__class__.cached_runs = {}
+		cache = model.__class__.cached_runs
+		run_signature = hash(model)+hash(json.dumps(args[1:]))+hash(json.dumps(kwargs))
+		if run_signature not in cache:
+			run_args = kwargs['run'] if 'run' in kwargs else {}
+			model.run(**run_args)
+			cache[run_signature] = (datetime.now(),model.__dict__.copy())
+		else:
+			model.__dict__.update(cache[run_signature][1])
+			#print model.x
+		return func(*args, **kwargs)
+	return decorate
+
+class CachingExample(UniformModel):
+	
+	def __init__(self, a, b, name=None):
+		self.x = None
+		super(CachingExample, self).__init__(a, b, name=name)
+
+	@runcache
+	def produce_number(self,**kwargs):
+		return self.x
+
+	def run(self,**kwargs):
+		if 'b' in kwargs:
+			self.x = uniform(self.a, self.b)
+
