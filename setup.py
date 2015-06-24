39cedad69939b4f6ad11361a1f870c922e62036d
Richard C Gerkin <rick1@gerkin.org>
2015-06-24 16:07:55 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='sciunit',
-	version='0.1.2',
+	version='0.1.3',
 	author='Rick Gerkin',
 	author_email='rgerkin@asu.edu',
 	packages=['sciunit', 'sciunit.tests'],
