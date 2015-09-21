ffb6d806e35c1425a7528b3b47625d0401f73e7b
Richard C Gerkin <rgerkin@asu.edu>
2015-09-21 14:30:52 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
 	name='sciunit',
-	version='0.1.4',
+	version='0.1.4.1',
 	author='Rick Gerkin',
 	author_email='rgerkin@asu.edu',
 	packages=['sciunit', 'sciunit.tests'],
