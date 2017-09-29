beff3b755ed98f9de8b79f349a312bb168a0b99b
rgerkin <rgerkin@asu.edu>
2017-09-29 12:21:05 -0700

    Added recursive import test

--- unit_test/core_tests.py
+++ unit_test/core_tests.py
@@ -8,7 +8,7 @@
 
 import unittest
 
-from sciunit.utils import NotebookTools
+from sciunit.utils import NotebookTools, import_all_modules
 
 
 class DocumentationTestCase(NotebookTools,unittest.TestCase):
@@ -28,6 +28,11 @@ class DocumentationTestCase(NotebookTools,unittest.TestCase):
 class ImportTestCase(unittest.TestCase):
     def test_quantities(self):
         import quantities
+
+    def test_import_everything(self):
+        import sciunit
+        # Recursively import all submodules
+        import_all_modules(sciunit)
 
 
 if __name__ == '__main__':
