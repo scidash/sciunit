515e979d726af9031a4689dae899c6f87cc22e65
Rick Gerkin <rgerkin@asu.edu>
2018-03-26 13:10:04 -0700

    Use find_packages() to determine packages to install

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ import os
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 # IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2
 if sys.version_info < (3,3):
@@ -29,9 +26,7 @@ setup(
     version='0.19',
     author='Rick Gerkin',
     author_email='rgerkin@asu.edu',
-    packages=['sciunit',
-              'sciunit.scores',
-              'sciunit.unit_test'],
+    packages=find_packages(),
     url='http://sciunit.scidash.org',
     license='MIT',
     description='A test-driven framework for formally validating scientific models against data.',
