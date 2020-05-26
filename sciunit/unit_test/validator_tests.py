a34fac317c9b09c3d516238cabda5e99a8cec907
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-05-25 18:04:21 -0700

    Add assert to validator test cases

--- sciunit/unit_test/validator_tests.py
+++ sciunit/unit_test/validator_tests.py
@@ -1,12 +1,26 @@
 import unittest
 import quantities as pq
+
 class ValidatorTestCase(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(1, 1)
+
     def register_test(self):
+
         class TestClass():
             intValue = 0
             def getIntValue(self):
                 return self.intValue
+
         from sciunit.validators import register_quantity, register_type
+        from cerberus import TypeDefinition, Validator
+
         register_type(TestClass, "TestType1")
         q = pq.Quantity([1,2,3], 'J')
         register_quantity(q, "TestType2")
+        self.assertTrue(isinstance(Validator.types_mapping['TestType1'], TypeDefinition))
+        self.assertTrue(isinstance(Validator.types_mapping['TestType2'], TypeDefinition))
+
+if __name__ == '__main__':
+    unittest.main()
