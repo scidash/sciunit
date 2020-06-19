8162997d207324545f9ddc440f840934201b0df1
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2020-06-19 11:05:27 +0200

    Update complete.py

--- sciunit/scores/complete.py
+++ sciunit/scores/complete.py
@@ -268,7 +268,7 @@ class RandomScore(FloatScore):
 
 class CorrelationScore(sciunit.scores.Score):
     """A correlation score.
-    A float in the range [-1.0,1.0] representing the correlation coefficient.
+    A float in the range [-1.0, 1.0] representing the correlation coefficient.
     """
 
     _description = ('A correlation of -1.0 shows a perfect negative correlation,'
@@ -276,15 +276,18 @@ class CorrelationScore(sciunit.scores.Score):
                     'A correlation of 0.0 shows no linear relationship between the movement of the two variables')
 
     _best = 1.0
+    
+    _worst = -1.0
 
     def _check_score(self, score):
         if not (-1.0 <= score <= 1.0):
-            raise errors.InvalidScoreError(("Score of %f must be in "
-                                            "range -1.0-1.0" % score))
+            raise errors.InvalidScoreError(("Score of %.3g must be in "
+                                            "range [-1.0, 1.0]" % score))
     @classmethod
     def compute(cls, observation, prediction):
         """Compute whether the observation equals the prediction."""
-        return CorrelationScore(float(np.corrcoef(observation, prediction)[0,1]))
+        return CorrelationScore(float(np.corrcoef(observation, prediction)[0, 1]))
 
     def __str__(self):
         return '%.3g' % self.score
+    
