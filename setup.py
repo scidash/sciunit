3cc6431ffb218a2e5dac31379637038bda8df2e9
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-06-26 18:03:26 -0700

    Use PEP 517/518

--- setup.py
+++ setup.py
@@ -1,44 +1,6 @@
-"""Setup file for SciUnit"""
+#!/usr/bin/env python
 
-from pathlib import Path
-from setuptools import setup, find_packages
+import setuptools
 
-
-def read_requirements() -> list:
-    '''parses requirements from requirements.txt'''
-    reqs_path = Path(__file__).parent / 'requirements.txt'
-    reqs = None
-    with open(reqs_path) as reqs_file:
-        reqs = reqs_file.read().splitlines()
-    return reqs
-
-def get_version() -> dict:
-    version = {}
-    with open(Path(__file__).parent / 'sciunit' / 'version.py') as f:
-        exec(f.read(), version)
-    return version['__version__']
-
-
-readme_path = Path(__file__).parent / 'README.md'
-with open(readme_path, encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='sciunit',
-    version=get_version(),
-    author='Rick Gerkin',
-    author_email='rgerkin@asu.edu',
-    packages=find_packages(),
-    url='http://sciunit.scidash.org',
-    license='MIT',
-    description=('A test-driven framework for formally validating scientific '
-                 'models against data.'),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=read_requirements(),
-    entry_points={
-        'console_scripts': [
-            'sciunit = sciunit.__main__:main'
-            ]
-        }
-    )
+if __name__ == "__main__":
+    setuptools.setup()
