1e42b320c13767ece23baa947e6520e8313fcc6a
Zhiwei Liang <43905414+ChihweiLHBird@users.noreply.github.com>
2020-08-18 02:02:47 -0700

    Add config_set, config_get in __init__.py of the project.

--- sciunit/__init__.py
+++ sciunit/__init__.py
@@ -7,7 +7,7 @@ Quantitative Scientific Models
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .utils import RUNTIME_SETTINGS, log
+from .utils import RUNTIME_SETTINGS, log, config_set, config_get
 from .models import Model
 from .capabilities import Capability
 from .tests import Test, TestM2M
