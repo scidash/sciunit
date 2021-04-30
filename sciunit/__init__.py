5648a474de09fd31fcda786cdae7334e04731aad
Richard Gerkin <rgerkin@asu.edu>
2021-04-29 23:10:19 -0700

    Error handling for versioning

--- sciunit/__init__.py
+++ sciunit/__init__.py
@@ -4,26 +4,25 @@ A Testing Framework for Data-Driven Validation of
 Quantitative Scientific Models
 """
 
+import logging
 import sys
 
 from .capabilities import Capability
 from .errors import Error
 from .models import Model
 from .scores import Score
-from .scores.collections import ScoreArray, ScoreMatrix  # , ScorePanel
+from .scores.collections import ScoreArray, ScoreMatrix
 from .scores.collections_m2m import ScoreArrayM2M, ScoreMatrixM2M
 from .suites import TestSuite
 from .tests import Test, TestM2M
 from .utils import RUNTIME_SETTINGS, config_get, config_set, log
 
-if sys.version_info[1] <= 7:
-    from importlib_metadata import version
-else:
+
+try:
     from importlib.metadata import version
-
-__version__ = version("sciunit")
-
-import logging
-
+    __version__ = version("sciunit")
+except:
+    __version__ = None
+    
 logger = logging.getLogger("sciunit")
 logger.setLevel(logging.WARNING)
